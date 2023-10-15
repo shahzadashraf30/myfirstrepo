547ed7b05f80d9479b017d614e0cf987db6863ff
shahzadashraf30 <148014101+shahzadashraf30@users.noreply.github.com>
2023-10-16 00:18:08 +0200

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Objectives:
+# - Data Science Tools and Ecosystem are summarized
+# - popular languages that Data Scientists use.
+# - commonly used libraries used by Data Scientists.
+# - simple arithmetic expression to mutiply then add integers.
+# - minutes to hours by diving
+
+# Some of the popular languages that Data Scientists use are:
+
+# 1. Python
+# 2. SQL (Structured Query Language)
+# 3. R
+# 4. Julia
+# 5. JavaScript
+# 6. Scala
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+# 1. Scrapy
+# 2. Pandas
+# 3. NumPy
+# 4. Scikit learn
+# 5. Matplotlib
+# 6. Plotly
+# 
+
+# | Data Science Tools |
+# | --- |
+# | Anaconda |
+# | Google Cloud Platform |
+# | Jupyter Notebooks |
+# 
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[13]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+
+
+# In[14]:
+
+
+(3*4)+5
+
+
+# In[16]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+
+
+# In[17]:
+
+
+200/60
+
+
+# ## Author
+
+# M Shahzad Ashraf
+
+# In[ ]:
+
+
+
+
